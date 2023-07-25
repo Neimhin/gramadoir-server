6996800c6dfbf80cabfa74a5191cf7d8bdd8f06c
Neimhin <nrobinso@tcd.ie>
2023-07-25 18:07:05 +0100

    breaking down the pos tagger

--- tools/install_service.sh
+++ tools/install_service.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 SERVICE_NAME=gramadoir-server
-sudo cp systemd-service /etc/systemd/system/$SERVICE_NAME.service
+sudo ln -s `realpath systemd-service` /etc/systemd/system/$SERVICE_NAME.service
+sudo systemctl daemon-reload
 sudo systemctl enable --now $SERVICE_NAME
 
 
 # e.g.
-curl localhost:10002/mo%20madra
+curl localhost:10002?text=mo%20madra
